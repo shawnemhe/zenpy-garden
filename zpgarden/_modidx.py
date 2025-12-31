90d5c5b94d36a659c248556f20b5ea117e5bbf84
Solveit <nobody@fast.ai>
2025-12-31 04:51:12 +0000

    add anchor component

--- zpgarden/_modidx.py
+++ zpgarden/_modidx.py
@@ -5,6 +5,7 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://shawnemhe.github.io',
                 'git_url': 'https://github.com/shawnemhe/zenpy-garden',
                 'lib_path': 'zpgarden'},
-  'syms': { 'zpgarden.core': { 'zpgarden.core._clstoname': ('core.html#_clstoname', 'zpgarden/core.py'),
+  'syms': { 'zpgarden.components': {},
+            'zpgarden.core': { 'zpgarden.core._clstoname': ('core.html#_clstoname', 'zpgarden/core.py'),
                                'zpgarden.core.mk_compfn': ('core.html#mk_compfn', 'zpgarden/core.py'),
                                'zpgarden.core.mk_previewer': ('core.html#mk_previewer', 'zpgarden/core.py')}}}
